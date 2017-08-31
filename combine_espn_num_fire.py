2db8cce108fff276e1ce11a0854294a244f10e76
Evan Racah <ejracah@gmail.com>
2017-08-30 20:08:29 -0400

    new updates for 2017

--- combine_espn_num_fire.py
+++ combine_espn_num_fire.py
@@ -4,14 +4,16 @@ import json
 
 d = {}
 lines=[]
-with open('espn_prices.csv', 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
+with open('espn_prices.csv', 'rU') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',',dialect=csv.excel_tab)
     for idx, row in enumerate(reader):
-        if idx == 0:
-            fieldnames = row
-        else:
-            player, pos, _, _, _, price = row
-            player = player.lower().split(' jr.')[0]
+            pos,player,price = row
+            for p in ["RB","WR","QB","TE","DST","K"]:
+                if p in pos:
+                    pos = p
+            player = player.lower().split('jr')[0].split('sr')[0]
+            player = player.split("d/st")[0]
+            player = player.replace(".","")
 
             if price == '--':
                 d[player] = {'price':1}
@@ -19,18 +21,22 @@ with open('espn_prices.csv', 'rb') as csvfile:
                 d[player] = {'price': price.replace('$','')}
             d[player]['pos'] = pos
 
+            
 
 
-with open('num_fire_points.csv', 'rb') as csvfile:
+
+with open('num_fire_points.csv', 'rU') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for idx, row in enumerate(reader):
         if idx == 0:
             fieldnames = row
         else:
-            player, _, _, _, risk, points, firefactor = row
-            player = player.lower()
+            firefactor,points,player = row
+            player = player.lower().split('jr')[0]
+            player = player.split("d/st")[0]
+            player = player.replace(".","")
+
             if player in d:
-                d[player]['risk'] = risk
                 d[player]['points'] = points
                 d[player]['firefactor'] = firefactor
             else:
@@ -42,6 +48,7 @@ for k in d.keys():
     d[k]['player'] = k
 
 dic = d.values()
+print d["tyhilton"]
 with open('data.csv', 'wb') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     writer.writerow(dic[0].keys())
